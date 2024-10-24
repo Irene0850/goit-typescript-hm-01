e594ce5f26f54e9d103b6628cba8ca97ed56696a
Iryna Kolomiichenko <159377221+Irene0850@users.noreply.github.com>
2024-10-24 22:53:06 +0300

    new basic

---
+++ vite-project/src/basic/4.ts
@@ -0,0 +1,14 @@
+function showMessage(message: string): void {
+  console.log(message);
+}
+showMessage("Hello");
+
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
+}
+calc(8, 8);
+
+function customError(): never {
+  throw new Error("Error");
+}
+console.log(customError);
