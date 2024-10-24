71772e09f707f28e2913f2871320a7813bb494b8
Iryna Kolomiichenko <159377221+Irene0850@users.noreply.github.com>
2024-10-24 22:30:32 +0300

    new

---
+++ vite-project/src/basic/1.ts
@@ -0,0 +1,13 @@
+const age = 50;
+const username = "Max";
+const toggle = true;
+const empty = null;
+const callback = (a: number): number => {
+  return 100 + a;
+};
+
+console.log(age);
+console.log(username);
+console.log(toggle);
+console.log(empty);
+console.log(callback);
---
+++ vite-project/src/basic/3.ts
@@ -0,0 +1,7 @@
+let mixType: string | number;
+mixType = "Hello";
+mixType = 25;
+
+let statusType: "enable" | "disable";
+statusType = "enable";
+statusType = "disable";
